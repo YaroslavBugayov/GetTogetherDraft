c9778584f86194eb39a643098a48ec1a41bde11f
YaroslavBugayov <bugayov576@gmail.com>
2022-09-29 19:27:29 +0300

    First step

---
+++ app/src/main/java/com/bobrbolt/gettogether/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bobrbolt.gettogether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
